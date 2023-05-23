bc83c6d9c9bcb43dd06b7fc02c4d9ad7a94b0ff8
Jano Bonnat <bonnat.jano.epet20@gmail.com>
2023-05-23 19:58:07 -0300

    commit de prueba

---
+++ proyectoplanilla/src/assets/imagenes.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-anonymous-default-export */
+/*import coraje from 'C:/proyectoplanilla/proyectoplanilla/src/assets/img/coraje.jpg';
+import unshowmas from 'C:/proyectoplanilla/proyectoplanilla/src/assets/img/unshowmas.jpg';*/
+import unshowmas from './img/unshowmas.jpg';
+import galaxia from './img/galaxia.jpg';
+import casa from './img/casa.png';
+
+
+export default {
+    "img1": unshowmas,
+    "img2": galaxia,
+    "img3": casa
+}
