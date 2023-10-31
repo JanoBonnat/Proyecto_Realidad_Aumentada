48edff6a28b484b26b685ca1b22c1418f4a00b1b
gomez jazmin <jazchugomez125@gmail.com>
2023-10-31 20:24:02 -0300

    Tercer intento

--- proyectoplanilla/src/QR/index.jsx
+++ proyectoplanilla/src/QR/index.jsx
@@ -1,16 +1,2 @@
-<script src="https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js"></script>
-<div id="qrcode"></div>
-<script>
-    // Obtén el elemento contenedor
-    var qrcodeContainer = document.getElementById("qrcode");
-    
-    // Contenido que deseas codificar en el código QR
-    var textoParaCodificar = "https://www.ejemplo.com";
-    
-    // Crea el código QR
-    var qrcode = new QRCode(qrcodeContainer, {
-        text: textoParaCodificar,
-        width: 128,
-        height: 128
-    });
-</script>
+import QRCode from "qrcode-generator";
+import React, { useRef, useEffect } from "react";
