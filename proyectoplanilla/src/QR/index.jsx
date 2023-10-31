1f0d1fdc480fcc282875a619ac60b6d0ead74dd6
gomez jazmin <jazchugomez125@gmail.com>
2023-10-31 20:25:14 -0300

    Se ingreso en la terminal el generador de qr

--- proyectoplanilla/src/QR/index.jsx
+++ proyectoplanilla/src/QR/index.jsx
@@ -1,2 +1,26 @@
 import QRCode from "qrcode-generator";
 import React, { useRef, useEffect } from "react";
+
+import React, { useRef, useEffect } from "react";
+
+const QRCodeComponent = ({ text }) => {
+  const qrCodeRef = useRef(null);
+
+  useEffect(() => {
+    // Crea el código QR
+    const qrCode = new QRCode(qrCodeRef.current, {
+      text: text,
+      width: 128,
+      height: 128
+    });
+
+    // Limpia el código QR cuando el componente se desmonta
+    return () => {
+      qrCode.clear();
+    };
+  }, [text]);
+
+  return <div ref={qrCodeRef}></div>;
+};
+
+export default QRCodeComponent;
