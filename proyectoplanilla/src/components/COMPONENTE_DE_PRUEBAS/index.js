021c66329a7b77019c7182f5ffc4a8ddcee3547c
Jano Bonnat <bonnat.jano.epet20@gmail.com>
2023-11-14 18:43:52 -0300

    Registro agregado.

--- proyectoplanilla/src/components/COMPONENTE_DE_PRUEBAS/index.js
+++ proyectoplanilla/src/components/COMPONENTE_DE_PRUEBAS/index.js
@@ -1,41 +1,13 @@
-//from https://divicio.us/effects/position-aware-hover-effect-for-divi-button/
-(function($) {
-    /* Position aware hover effect */
-    var ofs, x, y;
-    $(".btn.position_aware_effect").on("click", function(e) {
-      ofs = $(this).offset();
-      x = e.pageX - ofs.left;
-      y = e.pageY - ofs.top;
-      var name = $(this)
-      .text()
-      .toLowerCase()
-      .split(" ")[0];
-  
-      $(this).append(
-        '<div class="blob ' +
-        name +
-        '" style="left:' +
-        x +
-        "px; top: " +
-        y +
-        'px;"></div>'
-      );
-  
-      var blob = $(this).find(".blob");
-      setTimeout(function() {
-        blob.addClass("expand");
-      }, 20);
-    });
-    $(".btn.position_aware_effect").on("mouseleave", function(e) {
-      ofs = $(this).offset();
-      x = e.pageX - ofs.left;
-      y = e.pageY - ofs.top;
-      var blob = $(this).find(".blob");
-      blob.css({ left: x, top: y });
-      blob.removeClass("expand");
-      setTimeout(function() {
-        blob.remove();
-      }, 800);
-    });
-  })(jQuery);
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
   
--- proyectoplanilla/src/components/Login/index.jsx
+++ proyectoplanilla/src/components/Login/index.jsx
@@ -1,71 +1,148 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Input } from '../Input';
 import { Button } from '../Button';
-import  '../Estilos/style.css';
-/*import { Link } from 'react-router-dom';*/
-
 import { Auth } from '../../firebase/credenciales';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+import '../Estilos/style.css';
 
 const auth = getAuth(Auth);
 
-const Login = () => {;
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+};
+
+const Login = () => {
     const correoRef = useRef(null);
     const contraseñaRef = useRef(null);
+    const confirmarContraseñaRef = useRef(null);
+    const nombreRef = useRef(null); 
 
-    const funcAutenticacion = async (e) => {
+    const [registroError, setRegistroError] = useState(null); //Mensaje de error registro.
+
+    const validarNombre = () => {
+        const nombreUsuario = nombreRef.current?.value;
+        if (!expresiones.nombre.test(nombreUsuario)) {
+            setRegistroError("El nombre no puede contener números ni caracteres especiales");
+            return false;
+        } else {
+            setRegistroError(null);
+            return true;
+        }
+    };
+
+    const validarInputs = () => {
+        return validarNombre();
+    };
+
+    const resaltarErrores = () => {
+        const inputs = [nombreRef, correoRef, contraseñaRef, confirmarContraseñaRef];
+    
+        inputs.forEach((input) => {
+            if (input.current && !expresiones[input.current.id]?.test(input.current.value)) {
+                input.current.style.border = "5px solid #FF0000" ;
+            } else {
+                input.current.style.border = "1px solid #ced4da"; // Restaurar el estilo por defecto
+            }
+        });
+    };
+
+    const funcAutenticacion = async (e, registrando) => {
         e.preventDefault();
+
+        if (!validarInputs()) {
+            return;
+        }
 
         const correo = correoRef.current?.value;
         const contraseña = contraseñaRef.current?.value;
+        const confirmarContraseña = confirmarContraseñaRef.current?.value;
 
         try {
-            await signInWithEmailAndPassword(auth, correo, contraseña);
-            alert(`¡Bienvenido, ${correo}!`);
+            if (registrando) {
+
+                if (contraseña.length < 8) {
+                    setRegistroError("La contraseña debe tener al menos 8 caracteres");
+                    return;
+                }
+
+                if (contraseña !== confirmarContraseña) {
+                    alert("Las contraseñas no coinciden");
+                    return;
+                }
+
+                await createUserWithEmailAndPassword(auth, correo, contraseña);
+                alert(`¡Usuario registrado! ¡Bienvenido, ${correo}!`);
+            } 
+            
+            if (!registrando){
+                await signInWithEmailAndPassword(auth, correo, contraseña);
+                alert(`¡Bienvenido, ${correo}!`);
+            }
         } catch (error) {
-            alert("El correo o contraseña son inválidos");
+            alert(`Error de autenticación: ${error.message}`);
         }
     }
-    // INICIO ESTILOS.
-    /*
-    const padre = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+
+    //ESTILOS PROVISORIOS AHRE.
+    const loginStyle = {
+        marginBottom: '50px',
+        marginTop: '50px',
+        border: 'none',
     }
 
-    const bodyStyle = {
-        width: '600px',
-        height: '300px',
-        backgroundColor: '#FFE900',
-        boxShadow: '0 0 15px black',
-        textAlign: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
 
-    const submitStyle = {
-        marginTop: '10px',
-        width: '100px',
-        height: '40px',
-        border: '1px solid black',
-        borderRadius: '3px',
-    }
-    // FIN ESTILOS.
-*/
     return (
-        <div /*style={padre}*/>
-             <div /*style={bodyStyle}*/>
-                <form  onSubmit={funcAutenticacion}>
-                    <label>Correo</label>
-                    <Input placeholder="" id="email" ref={correoRef}/>
-                    <label>Contraseña</label>
-                    <Input placeholder="" type="password" id="contraseña" ref={contraseñaRef}/>
-                    <Button /*style={submitStyle}*/ type="submit" className="boton">Inicia Sesión</Button>
-                </form>
-            
-            </div>
+        < div className="container" id="container">
+                <div className="form-container sign-up">
+                    <form onSubmit={(e) => { e.preventDefault(); resaltarErrores(); funcAutenticacion(e, true); }} style={loginStyle}>
+                        <h1>Registro</h1>
+                        <label>Nombre y Apellido</label>
+                        <Input
+                            placeholder=""
+                            id="nombre"
+                            ref={nombreRef}
+                            onChange={() => validarNombre()}
+                        />
+                        <label>Correo</label>
+                        <Input placeholder="" id="correo" ref={correoRef} />
+                        <label>Contraseña</label>
+                        <Input placeholder="" type="password" id="contraseña" ref={contraseñaRef} />
+                        <label>Confirmar contraseña</label>
+                        <Input placeholder="" type="password" id="confirmarContraseña" ref={confirmarContraseñaRef} />
+                        <br></br>
+                        <Button type="submit" className="boton">Registrarse</Button>
+                        <p style={{ color: 'red' }}>{registroError}</p>
+                    </form>
+                </div>
+                <div className="form-container sign-in">
+                    <form onSubmit={(e) => { e.preventDefault(); resaltarErrores(); funcAutenticacion(e, false); }}>
+                        <h1>Login</h1>
+                        <label>Correo</label>
+                        <Input placeholder="" id="correo" ref={correoRef} />
+                        <label>Contraseña</label>
+                        <Input placeholder="" type="password" id="contraseña" ref={contraseñaRef} />
+                        <br></br>
+                        <Button type="submit" className="boton">Inicia Sesión</Button>
+                    </form>
+                </div>
+                <div className="toggle-container">
+                    <div className="toggle">
+                        <div className="toggle-panel toggle-left">
+                            <h1>Welcome Back!</h1>
+                            <p>Enter your personal details to use all of site features</p>
+                            <button className="hidden" id="login">Sign In</button>
+                        </div>
+                        <div className="toggle-panel toggle-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Register with your personal details to use all of site features</p>
+                            <button className="hidden" id="register">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
         </div>
     );
 }
