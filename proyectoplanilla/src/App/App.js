cb9c7a6d1d4011d399562b2e46eeea72c4b30664
MigueAsencio <miguel.asencio.epet20@gmail.com>
2023-11-16 00:49:06 -0300

    Se creó el nuevo menú.

--- proyectoplanilla/src/App/App.js
+++ proyectoplanilla/src/App/App.js
@@ -6,30 +6,28 @@ import { Login } from '../components/Login';
 import { Home } from "../components/Home";
 
 //importando módulos de firebase.
-import { Auth }  from '../firebase/credenciales'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { Auth } from '../firebase/credenciales'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 const auth = getAuth(Auth);
 
 
 
 function App() {
 
-  const [usuario, setUsuario] = useState(null);
+    const [usuario, setUsuario] = useState(null);
 
-  onAuthStateChanged(auth, (usuarioFirebase) => {
-    if(usuarioFirebase){
-      setUsuario(usuarioFirebase);
-    }
-    else{
-      setUsuario(null);
-    }
-  })
+    onAuthStateChanged(auth, (usuarioFirebase) => {
+        if (usuarioFirebase) {
+            setUsuario(usuarioFirebase);
+        } else {
+            setUsuario(null);
+        }
+    })
 
-  return (
-        <div className='container mt-2'>
-          {usuario ? <Home correoUsuario = {usuario.email} /> : <Login/>}
-        </div>
-  );
+    return ( <
+        div className = 'container mt-2' > { usuario ? < Home correoUsuario = { usuario.email } /> : <Login/ > } <
+        /div>
+    );
 }
 
 export default App;
